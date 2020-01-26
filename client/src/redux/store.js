634378127fd132def2fbf6d5af6c73155d3148da
m-moiz <muhammadmoiz856@hotmail.com>
2020-01-26 13:51:07 -0500

    update redux logger dev only solution

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -4,7 +4,13 @@ import rootReducer from './root-reducer';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { persistStore } from 'redux-persist';
 
-const middlewares = [ thunk, DEBUG && logger ].filter(Boolean);
+const middlewares = [ thunk ];
+
+if (process.env.NODE_ENV === 'development') {
+	const createLogger = require('redux-logger').createLogger;
+	const logger = createLogger();
+	middlewares.push(logger);
+}
 
 export default () => {
 	const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(...middlewares)));
