54bc2be835e402946386df49de3923d986b8c451
m-moiz <muhammadmoiz856@hotmail.com>
2020-01-26 13:34:27 -0500

    Made redux logger dev only

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,11 +1,16 @@
 import { createStore, applyMiddleware } from 'redux';
-import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import rootReducer from './root-reducer';
-import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import { persistStore } from 'redux-persist';
 
-const middlewares = [ logger, thunk ];
+const middlewares = [ thunk ];
+
+if (__DEV__) {
+	const createLogger = require('redux-logger').createLogger;
+	const logger = createLogger();
+	middlewares.push(logger);
+}
 
 export default () => {
 	const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(...middlewares)));
