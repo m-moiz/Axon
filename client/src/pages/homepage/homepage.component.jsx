81ef9e3bc12619aa6cd65aaf486d2428b767fbff
m-moiz <muhammadmoiz856@hotmail.com>
2020-02-09 18:51:49 -0500

    Fixed history typo

--- client/src/pages/homepage/homepage.component.jsx
+++ client/src/pages/homepage/homepage.component.jsx
@@ -9,7 +9,7 @@ import { withRouter } from 'react-router-dom';
 class Hompepage extends Component {
 	componentDidMount() {
 		if (this.props.isSignedIn === false || !window.sessionStorage.getItem('token')) {
-			this.history.push('/sign-in');
+			this.props.history.push('/sign-in');
 		}
 	}
 
