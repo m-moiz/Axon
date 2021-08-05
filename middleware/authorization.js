2f138912aa9b2e8fc407ac537cad01fc777b9d3f
m-moiz <muhammadmoiz856@hotmail.com>
2021-08-05 15:38:01 -0700

    redis

--- middleware/authorization.js
+++ middleware/authorization.js
@@ -2,6 +2,7 @@ const redisClient = require('../redis');
 
 exports.requireAuth = (req, res, next) => {
 	const { authorization } = req.headers;
+	console.log(authorization);
 	if (!authorization) {
 		return res.status(401).json({ message: 'Unauthorized' });
 	}
